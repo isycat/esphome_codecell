c22b425a6d7f451d6fbaa48209fb4d58fd6bab82
Ella <44198750+isycat@users.noreply.github.com>
2024-10-06 01:00:36 +0200

    Update and rename __init__.py to sensor.py

--- components/microbots_codecell/sensor.py
+++ components/microbots_codecell/sensor.py
@@ -1,12 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY
+from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY, UNIT_LUX, ICON_BRIGHTNESS_5
 
 microbots_codecell_ns = cg.esphome_ns.namespace('microbots_codecell')
 MyCodeCell = microbots_codecell_ns.class_('MyCodeCell', cg.PollingComponent)
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = sensor.sensor_schema(UNIT_LUX, ICON_BRIGHTNESS_5, 1).extend({
     cv.GenerateID(): cv.declare_id(MyCodeCell)
 }).extend(cv.polling_component_schema('10s'))
 
