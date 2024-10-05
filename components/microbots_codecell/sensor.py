76ea297e71f4c6cd2b3cb80b47e3ccd2e896ae52
Ella <44198750+isycat@users.noreply.github.com>
2024-10-06 01:23:21 +0200

    Update sensor.py

--- components/microbots_codecell/sensor.py
+++ components/microbots_codecell/sensor.py
@@ -6,9 +6,18 @@ from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY, UNIT_LUX, ICON_BRIGHT
 microbots_codecell_ns = cg.esphome_ns.namespace('microbots_codecell')
 MyCodeCell = microbots_codecell_ns.class_('MyCodeCell', sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_LUX, ICON_BRIGHTNESS_5, 1).extend({
-    cv.GenerateID(): cv.declare_id(MyCodeCell)
-}).extend(cv.polling_component_schema('10s'))
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        MyCodeCell,
+        # unit_of_measurement=UNIT_LUX,
+        # accuracy_decimals=1,
+        device_class=DEVICE_CLASS_ILLUMINANCE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    )
+    .extend({
+        cv.GenerateID(): cv.declare_id(MyCodeCell)
+    }).extend(cv.polling_component_schema('10s'))
+)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
