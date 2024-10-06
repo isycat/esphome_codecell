b87240b6dbd3250fbf62aaf610106d4b40003a97
Ella <44198750+isycat@users.noreply.github.com>
2024-10-06 02:30:32 +0200

    Update __init__.py

--- components/codecell/__init__.py
+++ components/codecell/__init__.py
@@ -8,20 +8,16 @@ AUTO_LOAD = ['sensor']
 codecell_ns = cg.esphome_ns.namespace('codecell')
 CodeCellSensor = codecell_ns.class_('CodeCellSensor', sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(CodeCellSensor)
-    })\
-    .extend(cv.polling_component_schema('10s'))\
-    .extend(
-        sensor.sensor_schema(
-            icon=ICON_BRIGHTNESS_5,
-            # unit_of_measurement=UNIT_LUX,
-            # accuracy_decimals=1,
-            device_class=DEVICE_CLASS_ILLUMINANCE,
-            state_class=STATE_CLASS_MEASUREMENT,
-        )
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        CodeCellSensor,
+        # unit_of_measurement=UNIT_LUX,
+        # accuracy_decimals=1,
+        device_class=DEVICE_CLASS_ILLUMINANCE,
+        state_class=STATE_CLASS_MEASUREMENT,
     )
+    .extend(cv.polling_component_schema('10s'))
+)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
